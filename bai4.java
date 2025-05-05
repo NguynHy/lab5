11dc69b888f3d93f655ecc362d7282c4f7c40d49
NguynHy <thathuy0509@gmail.com>
2025-05-05 10:52:46 +0700

    Create bai4.java

---
+++ bai4.java
@@ -0,0 +1,26 @@
+package LABTUAN5;
+import java.util.Scanner;
+
+public class Mainbai4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhap thong tin hinh tron:");
+        MovableCircle circle = new MovableCircle(0, 0, 0, 0, 0);
+        circle.nhap();
+
+        System.out.println("Vi tri ban dau: " + circle.toString());
+
+        circle.moveUp();
+        System.out.println("Sau khi di chuyen len: " + circle.toString());
+
+        circle.moveDown();
+        System.out.println("Sau khi di chuyen xuong: " + circle.toString());
+
+        circle.moveLeft();
+        System.out.println("Sau khi di chuyen sang trai: " + circle.toString());
+
+        circle.moveRight();
+        System.out.println("Sau khi di chuyen sang phai: " + circle.toString());
+    }
+}
